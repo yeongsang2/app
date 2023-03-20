acdf42818999e97b9fa2663c2f1561d05d1fcc13
yeongsang2 <dudtkd1221@gmail.com>
2023-03-20 15:17:29 +0900

    fix : modify api reponse, endpoint

--- app/main.py
+++ app/main.py
@@ -14,38 +14,24 @@ from color_classification import color
 
 
 app = FastAPI()
+UPLOAD_DIR = "/user/app/image/"  # 이미지를 저장할 서버 경로
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"ping"}
 
-@app.post("/object-to-json")
+@app.post("/clothes-classify")
 async def detect_clothes_return_json_result(file: bytes = File(...)):
 
-    UPLOAD_DIR = "/user/app/image/"  # 이미지를 저장할 서버 경로
-    
-    # content = await file.read()
     filename = f"{str(uuid.uuid4())}.jpg" 
-
     with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
         fp.write(file)
 
-    que = Queue()
-    que = color.execute(UPLOAD_DIR + filename, que)
+    color = get_color(file)
+    logo = get_logo(file)
 
-    print(que.get())
-
-    input_image = get_image_from_bytes(file)
-    model = get_yolov5()
-    results = model(input_image)
-    detect_res = results.pandas().xyxy[0].to_json(orient="records")
-    detect_res = json.loads(detect_res)
-    return {"result": detect_res}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+    result = {'logo': logo, 'color': color}
+    return {"result": result}
 
 def get_yolov5():
     model = torch.hub.load('/user/app/yolov5', 'custom', path='/user/app/yolov5/north.pt', source='local')
@@ -62,3 +48,26 @@ def get_image_from_bytes(binary_image, max_size=1024):
         int(input_image.height * resize_factor)
     ))
     return resized_image
+
+def get_logo(file):
+
+    input_image = get_image_from_bytes(file)
+    model = get_yolov5()
+    results = model(input_image)
+    detect_res = results.pandas().xyxy[0].to_json(orient="records")
+    print(detect_res)
+    detect_res = json.loads(detect_res)
+    if(detect_res):
+        return detect_res[0].get("name")
+    return
+            
+
+def get_color(file):
+
+    filename = f"{str(uuid.uuid4())}.jpg" 
+    with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+        fp.write(file)
+    
+    que = Queue()
+    que = color.execute(UPLOAD_DIR + filename, que)
+    return que.get()
